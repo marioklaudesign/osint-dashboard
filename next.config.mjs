992ae33a47ce17c35d951504d904e9725439c260
marioklaudesign <mario.klau1812@gmail.com>
2026-05-18 16:39:11 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
+    // Prevents ESLint errors from breaking production builds
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Prevents TypeScript syntax strict checks from breaking production builds
     ignoreBuildErrors: true,
   }
 };
